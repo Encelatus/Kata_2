1b56868a5379de73d99cc4c9280d1119614d4869
Samuel Tenschert <samuel.tenschert@gmail.com>
2026-05-25 11:49:22 +0200

    step 5: failing test -> single 3 returns 2 (Constanze)

--- electrons_around_the_cores/spec/electrons_spec.js
+++ electrons_around_the_cores/spec/electrons_spec.js
@@ -10,4 +10,8 @@ describe("Electrons around the cores", function () {
   it("should return 0 if no 3s or 5s in array", function () {
     expect(game.count([1, 2, 4, 6])).toEqual(0);
   });
+
+  it("should return 2 for a single 3", function () {
+    expect(game.count([3])).toEqual(2);
+  });
 });
