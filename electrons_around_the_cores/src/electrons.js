8fb072dd5177e829750ba98f89e3f97a0a0dee33
Samuel Tenschert <samuel.tenschert@gmail.com>
2026-05-25 12:02:12 +0200

    step 10: sum up electrons for multiple dice (Sam)

--- electrons_around_the_cores/src/electrons.js
+++ electrons_around_the_cores/src/electrons.js
@@ -1,12 +1,13 @@
 function ElectronsGame() {}
 ElectronsGame.prototype.count = function(dice) {
+  let totalElectrons = 0;
   for ( let i = 0; i < dice.length; i++) {
     if (dice[i] === 3) {
-      return 2;
+      totalElectrons += 2;
     }
     if (dice[i] === 5) {
-      return 4;
+      totalElectrons += 4;
     }
   }
-  return 0;
+  return totalElectrons;
 };
