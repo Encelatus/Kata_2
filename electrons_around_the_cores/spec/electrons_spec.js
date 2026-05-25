70e1541500c3de4b453a26e1a2e8eb8c982410f6
Samuel Tenschert <samuel.tenschert@gmail.com>
2026-05-25 11:27:45 +0200

    step 2: failing test->empty array returns 0 (Constanze)

--- electrons_around_the_cores/spec/electrons_spec.js
+++ electrons_around_the_cores/spec/electrons_spec.js
@@ -2,4 +2,8 @@ describe('Electrons around the cores', function() {
   beforeEach(function() {
     game = new ElectronsGame();
   });
+
+  it('should return 0 for an empty array', function() {
+    expect(game.count([])).toEqual(0);
+  });
 });
