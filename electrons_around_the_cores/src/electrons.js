5f6f6c5d3b8552d50ee79706ad7b6d2414453cf6
Samuel Tenschert <samuel.tenschert@gmail.com>
2026-05-25 11:53:35 +0200

    step 6: single 3 returns 2 (Sam)

--- electrons_around_the_cores/src/electrons.js
+++ electrons_around_the_cores/src/electrons.js
@@ -1,4 +1,9 @@
 function ElectronsGame() {}
 ElectronsGame.prototype.count = function(dice) {
+  for ( let i = 0; i < dice.length; i++) {
+    if (dice[i] === 3) {
+      return 2;
+    }
+  }
   return 0;
 };
