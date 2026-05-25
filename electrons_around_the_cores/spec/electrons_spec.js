4b18a563d94234680750e6a87770dd88eaf76e17
Samuel Tenschert <samuel.tenschert@gmail.com>
2026-05-25 11:43:11 +0200

    step 4: (successful) test -> no 3s or 5s returns 0 (Constanze)

--- electrons_around_the_cores/spec/electrons_spec.js
+++ electrons_around_the_cores/spec/electrons_spec.js
@@ -1,9 +1,13 @@
-describe('Electrons around the cores', function() {
-  beforeEach(function() {
+describe("Electrons around the cores", function () {
+  beforeEach(function () {
     game = new ElectronsGame();
   });
 
-  it('should return 0 for an empty array', function() {
+  it("should return 0 for an empty array", function () {
     expect(game.count([])).toEqual(0);
+  });
+
+  it("should return 0 if no 3s or 5s in array", function () {
+    expect(game.count([1, 2, 4, 6])).toEqual(0);
   });
 });
